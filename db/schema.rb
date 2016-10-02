b6d4e33f91c6d4052db594782398fcdf47d941ce
Caio Cruz <ctccaio@gmail.com>
2016-10-02 16:08:32 +0000

    Acertando a conexao com o BD

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140707111715) do
+ActiveRecord::Schema.define(version: 20161002152642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "usuarios", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "access_token"
+    t.string   "uid"
+    t.string   "photo_url"
+    t.string   "provider"
+    t.boolean  "status"
+    t.string   "reason"
+    t.string   "user_type"
+    t.date     "data_ingresso"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "widgets", force: :cascade do |t|
     t.string   "name"
