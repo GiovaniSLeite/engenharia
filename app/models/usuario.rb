b4b06ca9dcdb2b5ead81e687ca3b150d322f5d15
Caio Cruz <ctccaio@gmail.com>
2016-10-04 02:39:31 +0000

    Corrigindo Autenticacao Facebook

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -4,9 +4,8 @@ class Usuario < ActiveRecord::Base
 		
 		usuario = self.find_or_initialize_by(provider: auth.provider, uid: auth.uid)
 		
-		if usuario == nil
-			usuario.assign_attributes({ name: auth.info.name, email: auth.info.email, photo_url: auth.info.image, access_token: auth.credentials.token, status: true, user_type: "2", data_ingresso: Time.now})
-		end
+		usuario.assign_attributes({ name: auth.info.name, email: auth.info.email, photo_url: auth.info.image, access_token: auth.credentials.token, status: true, user_type: "2", data_ingresso: Time.now})
+		usuario.save
 		
 		usuario
 	end
