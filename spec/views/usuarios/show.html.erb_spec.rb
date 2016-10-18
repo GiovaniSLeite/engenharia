bbd06f518fbabab9fe1ff234f21bc2942c5f84d5
Caio Cruz <ctccaio@gmail.com>
2016-10-18 03:31:42 +0000

    Novos ajustes nas views

--- spec/views/usuarios/show.html.erb_spec.rb
+++ spec/views/usuarios/show.html.erb_spec.rb
@@ -8,10 +8,10 @@ RSpec.describe "usuarios/show", type: :view do
       :access_token => "Access Token",
       :uid => "Uid",
       :photo_url => "Photo Url",
-      :provider => "Provider",
+      :provider => "facebook",
       :status => false,
       :reason => "Reason",
-      :user_type => "User Type"
+      :user_type => "2"
     ))
   end
 
@@ -19,12 +19,10 @@ RSpec.describe "usuarios/show", type: :view do
     render
     expect(rendered).to match(/Name/)
     expect(rendered).to match("email@example.com")
-    expect(rendered).to match(/Access Token/)
     expect(rendered).to match(/Uid/)
-    expect(rendered).to match(/Photo Url/)
-    expect(rendered).to match(/Provider/)
-    expect(rendered).to match(/false/)
+    expect(rendered).to match(/Facebook/)
+    expect(rendered).to match(/Inativo/)
     expect(rendered).to match(/Reason/)
-    expect(rendered).to match(/User Type/)
+    expect(rendered).to match(/Apostador/)
   end
 end
